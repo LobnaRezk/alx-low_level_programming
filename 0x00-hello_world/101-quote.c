3fcea099c79bfd3657478487cdb17dd331129327
LobnaRezk <Lubna2018170788@cis.asu.edu.eg>
2023-03-17 09:31:55 -0700

    a

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Prints out the last part of a quote in the standard error.
+ * main - Entry point
+ * Description: Prints out the last part of a quote in the standard error.
  * Return: 1 if success.
  */
 int main(void)
