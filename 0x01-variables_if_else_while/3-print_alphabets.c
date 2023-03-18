18ad0ab456acff274ccbde99589a78fbf8226370
LobnaRezk <Lubna2018170788@cis.asu.edu.eg>
2023-03-18 15:53:14 -0700

    a

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+/**
+ * main- Entry point
+ *Description:print alphabet in lowercase then uppercase
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char ch = 'a';
+char CH = 'A';
+/*prints a-z */
+while (ch <= 'z')
+{
+putchar(ch);
+ch++;
+}
+while (CH <= 'Z')
+{
+putchar(CH);
+CH++;
+}
+putchar('\n');
+return (0);
+}
