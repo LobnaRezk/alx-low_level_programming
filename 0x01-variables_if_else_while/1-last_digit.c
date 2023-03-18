98a873064d7ba0f2caaa68fc36a78740e7ca7b6f
LobnaRezk <Lubna2018170788@cis.asu.edu.eg>
2023-03-18 14:38:05 -0700

    a

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,10 +11,9 @@
 
 int main(void)
 {
-int n;
+int n, digit;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-int digit;
 digit = n % 10;
 if (digit > 5)
 printf("Last digit of % i is % i and is greater than 5\n", n, digit);
