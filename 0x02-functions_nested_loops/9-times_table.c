a3489ecbdddbf3c9f4a16de88a18876ac5ec4c0a
LobnaRezk <Lubna2018170788@cis.asu.edu.eg>
2023-03-22 15:48:38 -0700

    a

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * times_table -  prints the 9 times table, starting with 0
+ * Return: Always 0 (Success)
+ */
+void times_table(void)
+{
+int r, c, prod;
+for (r = 0; r < 10; r++)
+{
+_putchar(48);
+for (c = 0; c < 10; c++)
+{
+_putchar(',');
+_putchar(' ');
+prod = r * c;
+
+if (prod <= 9)
+_putchar(' ');
+else
+_putchar((prod / 10) + 48);
+_putchar((prod % 10) + 48);
+}
+_putchar('\n');
+}
+}
