43324808698bd513723b282d1606130e9fbd1ca4
LobnaRezk <Lubna2018170788@cis.asu.edu.eg>
2023-03-24 17:10:26 -0700

    a

---
+++ 0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isupper - function that checks for uppercase character
+ * @c: is input for this function
+ * Return: returns 1 if c is upper Returns 0 otherwise
+ */
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+return (1);
+
+else
+
+return (0);
+
+}
