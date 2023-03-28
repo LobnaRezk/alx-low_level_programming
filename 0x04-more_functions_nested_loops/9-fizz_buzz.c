00860f70b260ba2467c808d54b226de4e6b4c170
LobnaRezk <Lubna2018170788@cis.asu.edu.eg>
2023-03-28 14:38:06 -0700

    a

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - function that checks for uppercase character.
+ * Return: 0
+ */
+int main(void)
+{
+int n = 100, i;
+for (i = 1; i <= n; i++)
+{
+if (i % 15 == 0)
+{
+printf("FizzBuzz ");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz ");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz ");
+}
+else
+{
+printf("%d ", i);
+}
+}
+printf('\n');
+return (0);
+}
