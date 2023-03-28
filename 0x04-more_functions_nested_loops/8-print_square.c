fae46ac012edd38352b3a6351349a95ed8c701c6
LobnaRezk <Lubna2018170788@cis.asu.edu.eg>
2023-03-28 14:14:36 -0700

    a

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,7 +6,7 @@
  */
 void print_square(int size)
 {
-int i;
+int i, j;
 if (size <= 0)
 {
 _putchar('\n');
@@ -15,8 +15,12 @@ else
 {
 for (i = 1; i <= size; i++)
 {
+	for (j = 1; j <= size; j++)
+	{
 	_putchar(35);
-}
+	}
+
 _putchar('\n');
+}
 }
 }
