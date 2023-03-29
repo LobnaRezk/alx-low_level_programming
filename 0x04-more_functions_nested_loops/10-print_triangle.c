f0e1aa2b1ad01b15efeff213c5f1a3a85a6ea28c
LobnaRezk <Lubna2018170788@cis.asu.edu.eg>
2023-03-29 07:32:21 -0700

    a

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,21 +6,31 @@
  */
 void print_triangle(int size)
 {
-int i, j, c;
-if (size > 0)
-{
-for (i = 1; i <= size; i++)
-{
-for (j = 1; j <= size; j++)
-{
-if (j <= size - i)
-_putchar(" ");
-else 
-_putchar(35);
-}
-_putchar('\n');
-}
-}
-else
-_putchar('\n');
+	int x, y = 0;
+	int space;
+
+	if (size > 0)
+	{
+		while (y < size)
+		{
+			space = size - y - 1;
+			while (x < size)
+			{
+				if (space > x)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+				x++;
+			}
+			y++;
+			x = 0;
+			_putchar ('\n');
+		}
+	}
+	else
+		_putchar('\n');
 }
