b71048b3312f5c07d69c780056ab8973f711b7c8
LobnaRezk <Lubna2018170788@cis.asu.edu.eg>
2023-04-03 06:58:32 -0700

    a

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *  _puts - function that prints a string, followed by a new line, to stdout
+ * @str: input of function is pointer
+ * Return: Always return 0 (Success)
+ */
+void _puts(char *str)
+{
+_putchar(*str'\n');
+}
