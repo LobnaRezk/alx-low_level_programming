88a954f2f21a6d7f365e66053d567538d5f46772
LobnaRezk <Lubna2018170788@cis.asu.edu.eg>
2023-04-03 08:30:33 -0700

    a

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * rev_string - function that prints a string, in reverse,
+ *               followed by a new line
+ * @s: input of function is pointer
+ * Return: Always return 0 (Success)
+ */
+void rev_string(char *s)
+{
+int l, i;
+char temp;
+for (l = 0; s[l] !='\0'; ++l)
+;
+for (i = 0; i < l / 2; i++)
+{
+temp = s[i];
+s[i] = s[l - 1 - i];
+s[l - 1 -i] = temp;
+}
+}
