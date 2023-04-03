a7337f038047f5ff44210d3a7445ef5e377d8194
LobnaRezk <Lubna2018170788@cis.asu.edu.eg>
2023-04-03 06:38:26 -0700

    a

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ *  _strlen -   function that returns the length of a stringe
+ * @s: input of function is pointer
+ * Return: return length of a string
+ */
+int _strlen(char *s)
+{
+int len = 0;
+len = sizeof(s);
+return (len);
+}
