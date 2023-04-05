d300fabc1f920c198b65fb7e3a684d88f9a7abcc
LobnaRezk <Lubna2018170788@cis.asu.edu.eg>
2023-04-05 16:06:17 -0700

    a

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,13 +7,8 @@
 void _puts_recursion(char *s)
 {
 /* size_t size = sizeof(s) / sizeof(s[0]);*/
-if (*s != '\0')
-{
-putchar('\n');
-}
-else
-{
+if (*s == '\0')
+return;
 putchar(*s);
 _puts_recursion(s + 1);
-}
 }
