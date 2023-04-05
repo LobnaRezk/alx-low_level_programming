e242f62ea46d6fa0b201316af4bbe793b33f5b89
LobnaRezk <Lubna2018170788@cis.asu.edu.eg>
2023-04-05 02:57:24 -0700

    a

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,15 +6,15 @@
  */
 void puts_half(char *str)
 {
-int len = 0;/* i*/
+int len = 0, i;
 while (*str != '\0')
 {
 len++;
 str++;
 }
 len++;
-/*i = len;*/
-for (len /= 2; str[len] != '\0'; ++len)
+i = len
+for (len /= 2; len < i; len++)
 {
 putchar(str[len]);
 }
