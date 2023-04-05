42c7859e54d312f8616b89f077b7404655ae46dd
LobnaRezk <Lubna2018170788@cis.asu.edu.eg>
2023-04-05 05:25:50 -0700

    a

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *_atoi -   function that copies the string pointed to by src,
+ * including the terminating null byte (\0), to the buffer pointed to by dest.
+ * @s: input of function is pointer
+ * Return: return the value 
+ */
+int _atoi(char *s)
+{
+unsigned int num = 0;
+int sign = 1;
+do {
+if (*s == '-')
+sign *= -1;
+else if (*s >= '0' && *s <= '9')
+num = (num * 10) + (*s - '0')
+else if (num > 0)
+break;
+
+} while (*s++);
+return (num * sign);
+}
