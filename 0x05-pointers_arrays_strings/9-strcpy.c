9582a7f9656e2f0866a35f8138fc1bde94003db2
LobnaRezk <Lubna2018170788@cis.asu.edu.eg>
2023-04-05 04:46:41 -0700

    a

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_array -   function that copies the string pointed to by src,
+ * including the terminating null byte (\0), to the buffer pointed to by dest.
+ * @dest: input of function is pointer
+ * @src: input of function is 
+ * Return: Always return 0 (Success)
+ */
+*_strcpy(char *dest, char *src)
+{
+*dest = *src;
+return (*src);
+}
