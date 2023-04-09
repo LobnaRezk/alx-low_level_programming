98358753e3acd0354748929c4a040df9c365385b
LobnaRezk <Lubna2018170788@cis.asu.edu.eg>
2023-04-09 11:28:05 -0700

    a

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,7 +14,7 @@ if (str[i] >= 'a' && str[i] <= 'z')
 if (str[i - 1] ==  44 || str[i - 1] ==  59  || str[i - 1] ==  46 ||
 str[i - 1] ==  33 || str[i - 1] == 63  ||  str[i - 1] == 34 || str[i - 1] == 40
 || str[i - 1] == 41 || str[i - 1] == 123 || str[i - 1] == 125)
-str[i] = str[i] - 32;
+str[i + 1] = str[i + 1] - 32;
 }
 }
 return (str);
