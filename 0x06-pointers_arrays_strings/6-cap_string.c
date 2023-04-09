1224ac41fb670a323650484771547711b99c2ebb
LobnaRezk <Lubna2018170788@cis.asu.edu.eg>
2023-04-09 11:07:04 -0700

    a

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,9 +9,9 @@ char *cap_string(char *str)
 int i;
 for (i = 0; str[i] != '\0'; i++)
 {
-if (str[i] ==   ',' || str[i] ==  ';'  || str[i] ==  '.' || str[i] ==   '!' ||
-str[i] ==   '?'   ||  str[i] == '"' || str[i] == '(' || str[i] == ')' ||
-str[i] == '{' || str[i] == '}')
+if (str[i] ==  44 || str[i] ==  59  || str[i] ==  46 || str[i] ==  33 ||
+str[i] == 63  ||  str[i] == 34 || str[i] == 40 || str[i] == 41 ||
+str[i] == 123 || str[i] == 125)
 str[i + 1] = str[i + 1] - 32;
 }
 
