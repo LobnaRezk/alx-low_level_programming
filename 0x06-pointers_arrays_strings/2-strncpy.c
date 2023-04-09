3f3f7c357ee42bb09da15ebf6b753741f0e3f5eb
LobnaRezk <Lubna2018170788@cis.asu.edu.eg>
2023-04-09 08:10:36 -0700

    a

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,15 +10,11 @@ char *_strncpy(char *dest, char *src, int n)
 {
 int  j = 0;
 for (; j < n && src[j] != '\0'; j++)
-{
-
 dest[j] = src[j];
 while (j < n)
 {
 dest[j] = '\0';
 j++;
-}
-
 }
 
 return (dest);
