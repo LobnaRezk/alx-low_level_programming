aa76472ddef7dc3d33d1565188ca14b2feac2285
LobnaRezk <Lubna2018170788@cis.asu.edu.eg>
2023-04-10 20:24:53 -0700

    a

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints the number of arguments passed into it
+ * @argc: count of char
+ * @argv:an arrat
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc);
+return (0);
+}
