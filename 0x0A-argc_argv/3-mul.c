c96bc9d59c08af0625bb59beaa5c3216a4bba12e
LobnaRezk <Lubna2018170788@cis.asu.edu.eg>
2023-04-17 06:16:50 -0700

    a

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,11 +10,14 @@ int main(int argc, char *argv[])
 {
 int result = 1;
 if ((argc - 1) < 2)
+{
 printf("Error\n");
+return (1);
+}
 else
 {
 result = atoi(argv[1]) * atoi(argv[2]);
 printf("%d\n", result);
 }
-return (result);
+return (0);
 }
