af21c905c40b6ce9a3ddfa3ae7cf401d4ec87cd3
LobnaRezk <Lubna2018170788@cis.asu.edu.eg>
2023-04-17 07:17:29 -0700

    a

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -19,7 +19,7 @@ while (--argc)
 {
 for (c = argv[argc]; *c ; c++)
 {
-if (*c < '0' && *c > '9')
+if (*c < '0' || *c > '9')
 {
 printf("Error\n");
 return (1);
@@ -27,8 +27,8 @@ return (1);
 }
 result = result + atoi(argv[argc]);
 }
-}
-printf("%d\n", result);
 
+printf("%d\n", result);
+}
 return (0);
 }
