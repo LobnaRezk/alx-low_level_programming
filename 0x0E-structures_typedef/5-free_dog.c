56b1c495856b84a5de31975c0ae2254c3fc4875f
LobnaRezk <Lubna2018170788@cis.asu.edu.eg>
2023-04-20 03:59:34 -0700

    a

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * free_dog - function that creates a new dog
+ * @d: pointer of char
+ * Return: 0 Always (Success)
+ */
+void free_dog(dog_t *d)
+{
+if (d)
+{
+free(d->owner);
+free(d->name);
+free(d);
+}    
+}
