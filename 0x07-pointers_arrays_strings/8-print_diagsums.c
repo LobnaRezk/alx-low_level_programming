6243d1b6b0d6e12a0daa5dd8bb2a860cba4e921b
LobnaRezk <Lubna2018170788@cis.asu.edu.eg>
2023-04-20 05:45:07 -0700

    a

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,15 +11,10 @@ void print_diagsums(int *a, int size)
 int i, j, sumx = 0, sumy = 0;
 for (i = 0; i < size; i++)
 {
-for (j = 0; j < size; j++)
-{
-if (i == j)
-sumx = sumx + a[i][j];
+sumx = sumx + a[i];
+sumy = sumy + a[size - i - 1];
+}
 
-if ((i + j) == (size - 1))
-sumy = sumy + a[i][j];
-}
-}
 printf("%d, %d", sumx, sumy);
 putchar('\n');
 }
