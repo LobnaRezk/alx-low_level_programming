2ae2a8cd24b1084c7a2308108077df77154da10a
LobnaRezk <Lubna2018170788@cis.asu.edu.eg>
2023-04-19 17:46:23 -0700

    a

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/**
+ * init_dog- function that initialize a variable of type struct dog
+ * @dog: struct
+ * @name: array of char
+ * @age: float
+ * @owner: array of char
+ * Return: 0 Always (Success)
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d.name = *name;
+d.age = age;
+d.owner = owner;
+}
