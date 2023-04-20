315c1ccb797d9d1258130e6b89091089ebd824ed
LobnaRezk <Lubna2018170788@cis.asu.edu.eg>
2023-04-20 02:10:32 -0700

    a

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -8,9 +8,9 @@
  */
 dog_t *new_dog(char *name, float age, char *owner);
 {
-dog_t *my_dog;
-my_dog->name = name;
-my_dog->age = age;
-my_dog->owner = owner;
-return (my_dog);
+dog_t *doggy;
+doggy->name = name;
+doggy->age = age;
+doggy->owner = owner;
+return (doggy);
 }
