2a45a94f1fd34db6be45a15d352e661946517d96
LobnaRezk <Lubna2018170788@cis.asu.edu.eg>
2023-04-21 13:13:21 -0700

    a

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,7 +15,7 @@ va_list ap;
 va_start(ap, n);
 for (i = 0; i < n; i++)
 {
-printf("%d", va_arg(ap, int);
+printf("%d", va_arg(ap, int));
 if (separator != NULL && i < n - 1)
 printf("%s", separator);
 }
