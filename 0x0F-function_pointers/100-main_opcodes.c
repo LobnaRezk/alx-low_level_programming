8848a1e9e2e2922f57142062e12d01bec09c30c1
LobnaRezk <Lubna2018170788@cis.asu.edu.eg>
2023-04-22 05:43:01 -0700

    a

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -9,12 +9,14 @@
   */
 int main(int argc, char  **argv[])
 {
-	char *p = (char *)main;
-	int c, i;
+	char  *p = (char *)main;
+	int  c, i;
+
 	if (argc != 2)
 		printf("Error\n"), exit(1);
+
 	c = atoi(*argv[1]);
-	if ( c < 0)
+	if (c < 0)
 		printf("Error\n"), exit(2);
 	for (i = 0; i < c; i++)
 		printf("%02hhx%s", *p++, c ? "" : "\n");
