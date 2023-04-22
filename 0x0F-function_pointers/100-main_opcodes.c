5408daa7bab5974a04a2b28faa2e136698c5c63c
LobnaRezk <Lubna2018170788@cis.asu.edu.eg>
2023-04-22 05:20:46 -0700

    a

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,11 +2,21 @@
 #include <stdarg.h>
 
 /**
-  * main - define function
-  * @void - describe argument
-  * Return: what does it return?
+  * main - prints the opcodes of its own main function.
+  * @argc:int
+  * @argv:array
+  * Return: int
   */
-int main(void)
+int main(int argc, char  **argv[])
 {
+	char *p = (char *)main;
+	int c, i;
+	if (argc != 2)
+		printf("Error\n"), exit(1);
+	c = atoi(argv[1]);
+	if ( c < 0)
+		printf("Error\n"), exit(2);
+	for (i = 0; i < c; i++)
+		printf("%02hhx%s", *p++, b ? "" : "\n");
 	return (0);
 }
