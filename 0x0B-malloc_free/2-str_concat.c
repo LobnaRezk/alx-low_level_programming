173e7227fb99796baea86f37ad1d0f970f6ecbfe
LobnaRezk <Lubna2018170788@cis.asu.edu.eg>
2023-05-01 03:09:40 -0700

    a

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,9 +13,9 @@ char *str_concat(char *s1, char *s2)
 char *p;
 int s1count, s2count, sizeBuffer, i;
 if (s1 == NULL)
-s1 == "";
+*s1 == "";
 if (s2 == NULL)
-s2 == "";
+*s2 == "";
 s1count = 0;
 while (s1[s1count] != '\0')
 {
