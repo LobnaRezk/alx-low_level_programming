4d3309add099b0bd40d4e60fbd432f3fbf280224
LobnaRezk <Lubna2018170788@cis.asu.edu.eg>
2023-05-02 16:01:35 -0700

    a

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * free_listint function that frees a listint_t list
+ * @head:const listint_t *head
+ * Return: size_t
+ */
+void free_listint(listint_t *head)
+{
+if (head == NULL)
+return;
+free_listint(head->next);
+free(head);
+}
