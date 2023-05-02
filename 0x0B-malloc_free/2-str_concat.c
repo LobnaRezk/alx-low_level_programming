619000702d92079bd35bc387b7329934dc703911
LobnaRezk <Lubna2018170788@cis.asu.edu.eg>
2023-05-02 01:04:24 -0700

    a

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,6 +3,21 @@
 #include <stdarg.h>
 #include <stdlib.h>
 /**
+ *  _strlen -   function that returns the length of a stringe
+ * @s: input of function is pointer
+ * Return: return length of a string
+ */
+int _strlen(char *s)
+{
+int len = 0;
+while (*s != '\0')
+{
+len++;
+s++;
+}
+return (len);
+}
+/**
  * str_concat -  function that concatenates two strings
  * @s1:char
  * @s2:char
@@ -16,18 +31,9 @@ if (s1 == NULL)
 s1 = "\0";
 if (s2 == NULL)
 s2 = "\0";
-s1count = 0;
-while (s1[s1count] != '\0')
-{
-s1++;
-s1count++;
-}
-s2count = 0;
-while (s2[s2count] != '\0')
-{
-s2++;
-s2count++;
-}
+s1count =  _strlen(s1);
+s2count =  _strlen(s2);
+
 p = malloc((s1count + s2count) *sizeof(char) + 1);
 if (p == NULL)
 return (0);
