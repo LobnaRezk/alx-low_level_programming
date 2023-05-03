b93e3dd5bae412939ad15d4a4c6193cfb8a9ff28
LobnaRezk <Lubna2018170788@cis.asu.edu.eg>
2023-05-03 04:13:17 -0700

    a

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -3,11 +3,11 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- *delete_nodeint_at_index- function that deletes the node at index 
+ *delete_nodeint_at_index- function that deletes the node at index
  *              index of a listint_t linked list
  * @head:const listint_t *head
  * @index:int
- * Return: int 
+ * Return: int
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
