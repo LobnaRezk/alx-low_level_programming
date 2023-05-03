f8826256d0c2c9e82ef5f4863ec845dc8cc9aa6e
LobnaRezk <Lubna2018170788@cis.asu.edu.eg>
2023-05-03 04:21:25 -0700

    a

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * reverse_listint- function that reverses a listint_t linked list.
+ * @head:const listint_t *head
+ * Return: listint_t
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+head++;
+return (0);
+}
